9511d40a34d7e0906731ae0b79144f010df0399f
Korendir19 <nollington01@gmail.com>
2021-04-17 12:05:25 +1000

    Patch 17042021.1204 v1
    Renamed variables, commented
    Added in comparisons, copy, find char

--- String.c
+++ String.c
@@ -2,35 +2,35 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+//String Construction
 typedef struct
 {
 	int length; //amount of chars that will be shown
 	int size; //amount of chars + null character
-	char* c; //list of chars
+	char* string; //list of chars
 } String;
-
 String* initString()
 {
 	String* s = (String*)malloc(sizeof(String));
 	s->length = 0;
 	s->size = s->length + 1;
-	s->c = (char*)malloc(s->size * sizeof(char));
+	s->string = (char*)malloc(s->size * sizeof(char));
 
 	return s;
 }
 
+//Finders, Setters, and Getters
 void findLength(String* s)
-{	
+{
 	String* tempString = s; //temporary string to manipulate
 	int counter; //counts through char array
-	for (counter = 0; tempString->c[counter] != '\0'; counter) {
+	for (counter = 0; tempString->string[counter] != '\0'; counter) {
 		counter++;
 	}
 	s->length = counter;
 	s->size = s->length + 1;
-
-	return s;
 }
 int returnLength(String* s)
 {
@@ -40,15 +40,40 @@ int returnSize(String* s)
 {
 	return s->size;
 }
-String* setString(char charSequence[])
+void setString(String* s, char charSequence[])
 {
-	String* s = initString();
-	s->c = charSequence, "\0";
+	s->string = charSequence, "\0";
 	findLength(s);
-
-	return s;
 }
 char* returnString(String* s)
 {
-	return s->c;
+	return s->string;
+}
+
+//Functions
+char* compareString(String* s1, String* s2)
+{
+	return s1->string == s2->string ? "true" : "false";
+}
+char* compareLength(String* s1, String* s2)
+{
+	return s1->length == s2->length ? "true" : "false";
+}
+void replaceString(String* s, char* newS)
+{
+	s->string = newS;
+}
+String* copyString(String* s)
+{
+	return s;
+}
+int findChar(String* s, char toFind)
+{
+	for (int i = 0; i < s->length; i++) {
+		if (s->string[i] == toFind) {
+			return i;
+		}
+	}
+
+	return -1;
 }
